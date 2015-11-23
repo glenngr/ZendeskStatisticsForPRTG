2b35ad8a1393a400e3c6b08637ff45ee16302fa1
glenngr <glenn@glenng.no>
2015-11-23 14:02:56 +0100

    Merge branch 'master' of https://github.com/glenngr/ZendeskStatisticsForPRTG
    Conflicts:
    	Common/app.config

--- Common/Configuration.Designer.cs
+++ Common/Configuration.Designer.cs
@@ -8,11 +8,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Common {
+namespace Common{
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Configuration : global::System.Configuration.ApplicationSettingsBase {
         
         private static Configuration defaultInstance = ((Configuration)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Configuration())));
